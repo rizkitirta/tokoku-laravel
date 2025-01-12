5bd0522e9f57ceaf56101aa84a062af6beea6eca
TirtaDev <75197912+rizkitirta@users.noreply.github.com>
2025-01-12 20:37:25 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,21 +1,64 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * Here is the serverless function entry
+ * for deployment with Vercel.
  */
 
 $uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+if ($uri !== '/' && file_exists($file = __DIR__.'/public'.$uri)) {
+    header('Content-type: '.get_mime_type($file).'; charset: UTF-8;');
+    echo require $file;
+} else {
+    require_once __DIR__.'/public/index.php';
 }
 
-require_once __DIR__.'/public/index.php';
+
+
+
+function get_mime_type($filename) {
+    $extension = strtolower(pathinfo($filename)['extension']);
+
+    $mimes = array(
+        'txt' => 'text/plain',
+        'html' => 'text/html',
+        'php' => 'text/html',
+        'css' => 'text/css',
+        'js' => 'application/javascript',
+        'json' => 'application/json',
+        'xml' => 'application/xml',
+        // images
+        'png' => 'image/png',
+        'jpe' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'jpg' => 'image/jpeg',
+        'webp' => 'image/webp',
+        'gif' => 'image/gif',
+        'bmp' => 'image/bmp',
+        'ico' => 'image/vnd.microsoft.icon',
+        'tiff' => 'image/tiff',
+        'tif' => 'image/tiff',
+        'svg' => 'image/svg+xml',
+        'svgz' => 'image/svg+xml',
+        // archives
+        'zip' => 'application/zip',
+        'rar' => 'application/x-rar-compressed',
+        // audio/video
+        'mp3' => 'audio/mpeg',
+        'qt' => 'video/quicktime',
+        'mov' => 'video/quicktime',
+        // fonts
+        'ttf' => 'application/x-font-ttf',
+        'woff' => 'application/x-woff',
+        'woff2' => 'font/woff2',
+        'otf' => 'font/otf',
+    );
+
+    if (isset($mimes[$extension])) {
+        return $mimes[$extension];
+    }
+    return 'application/octet-stream';
+}
