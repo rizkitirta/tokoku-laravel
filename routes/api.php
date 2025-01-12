011356b99f3c286116c11463ba6bb27d0d21e041
TirtaDev <75197912+rizkitirta@users.noreply.github.com>
2025-01-12 07:17:48 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('healt',function(){
+Route::get('/health',function(){
    return response()->json(['OK']);
 });
