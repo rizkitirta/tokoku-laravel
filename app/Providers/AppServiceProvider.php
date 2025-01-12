1d426ad11c8b209c2cf46dfb4cc749263a045403
TirtaDev <75197912+rizkitirta@users.noreply.github.com>
2025-01-12 21:03:11 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,7 +34,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('APP_ENV','local') !== 'local') {
-            URL::forceScheme('https');
+            // URL::forceScheme('https');
         }
     }
 }
